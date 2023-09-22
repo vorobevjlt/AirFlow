3524d955958d543c80a1a2a6e29ea9d6553957cc
vorobevjlt <127599181+vorobevjlt@users.noreply.github.com>
2023-09-22 20:45:05 +0500

    Update dag.py

--- airflow/dags/dag.py
+++ airflow/dags/dag.py
@@ -3,26 +3,22 @@ import sqlite3
 
 CON = sqlite3.connect('example.db')
 
-# Повторим предыдущий код с небольшим измененеием, теперь файлы 
-# записываются на диск, а не передаются в потоке исполнения
-
 def extract_data(url, tmp_file, **context):
     """ Extract CSV
     """
-    pd.read_csv(url).to_csv(tmp_file) # Изменение to_csv
+    pd.read_csv(url).to_csv(tmp_file) 
 
 
 def transform_data(group, agreg, tmp_file, tmp_agg_file, **context):
     """ Group by data
     """
-    data = pd.read_csv(tmp_file) # Изменение read_csv
-    data.groupby(group).agg(agreg).reset_index().to_csv(tmp_agg_file) # Изменение to_csv
- 
+    data = pd.read_csv(tmp_file)
+    data.groupby(group).agg(agreg).reset_index().to_csv(tmp_agg_file)
 
 def load_data(tmp_file, table_name, conn=CON, **context):
     """ Load to DB
     """
-    data = pd.read_csv(tmp_file)# Изменение read_csv
+    data = pd.read_csv(tmp_file)
     data["insert_time"] = pd.to_datetime("now")
     data.to_sql(table_name, conn, if_exists='replace', index=False)
 
@@ -32,19 +28,12 @@ from airflow.utils.dates import days_ago
 from airflow.operators.email_operator import EmailOperator
 from airflow.operators.python_operator import PythonOperator
 
-# Создаем DAG(контейнер) в который поместим наши задачи
-# Для DAG-а характерны следующие атрибуты
-# - Интервал запусков
-# - Начальная точка запуска
 with DAG(dag_id='dag',
          default_args={'owner': 'airflow'},
-         schedule_interval='@once', # Интервал запусков
-         start_date=days_ago(1) # Начальная точка запуска
+         schedule_interval='@once',
+         start_date=days_ago(1) 
     ) as dag:
-
- 
-    # Создадим задачу которая будет запускать питон функция
-    # Все именно так, создаем код для запуска другого кода
+        
     extract_data = PythonOperator(
         task_id='extract_data',
         python_callable=extract_data,
@@ -72,7 +61,6 @@ with DAG(dag_id='dag',
         }
     )
 
-    # Создадим задачу которая будет отправлять файл на почту
     email_op = EmailOperator(
         task_id='send_email',
         to="wayoutl1@yandex.ru",
@@ -81,6 +69,4 @@ with DAG(dag_id='dag',
         files=['/tmp/file_agg.csv']
     )
 
-    # Создадим порядок выполнения задач
-    # В данном случае 2 задачи буудт последователньы и ещё 2 парараллельны
     extract_data >> transform_data >> [load_data, email_op]
